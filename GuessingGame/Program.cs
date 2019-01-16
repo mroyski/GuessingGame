7bf1c0d9fe72a11dede2ed3dd54aac81564c756c
Mike <mroyski@gmail.com>
2019-01-16 13:15:03 -0500

    First iteration complete.

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,8 +6,34 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Adios!");
+            Console.WriteLine("Please guess a number between 1 and 10.");
+            string userGuess = Console.ReadLine();
+            Console.WriteLine("You guessed: " + userGuess);
+
+            //if (userGuess.Equals("7"))
+            //{
+            //    Console.WriteLine("You win!");
+            //}
+            //else if (userGuess.Equals("0"))
+            //{
+            //    Console.WriteLine("Please guess a number between 1 and 10.");
+            //} else
+            //{
+            //    Console.WriteLine("Try again.");
+            //}
+
+            switch (userGuess)
+            {
+                case "7":
+                    Console.WriteLine("You win!");
+                    break;
+                case "0":
+                    Console.WriteLine("Please guess a number between 1 and 10.");
+                    break;
+                default:
+                    Console.WriteLine("Try again.");
+                    break;
+            }
         }
     }
 }
