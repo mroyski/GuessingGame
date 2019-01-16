b1c2ab238ec21af00d89569be9ef04631bb75594
Mike <mroyski@gmail.com>
2019-01-16 14:12:05 -0500

    Added help option.

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,34 +6,45 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please guess a number between 1 and 10.");
+            Console.WriteLine("Please guess a number between 1 and 10, or enter 0 for HELP.");
             string userGuess = Console.ReadLine();
+
+            if (userGuess.Equals("0"))
+            {
+                Console.WriteLine("===== INSTRUCTIONS =====");
+                Console.WriteLine("Guess a number between 1 and 10.");
+                Console.WriteLine("Hit enter to submit your guess.");
+
+                userGuess = Console.ReadLine();
+            }
+
+
+
             Console.WriteLine("You guessed: " + userGuess);
 
-            //if (userGuess.Equals("7"))
-            //{
-            //    Console.WriteLine("You win!");
-            //}
-            //else if (userGuess.Equals("0"))
-            //{
-            //    Console.WriteLine("Please guess a number between 1 and 10.");
-            //} else
-            //{
-            //    Console.WriteLine("Try again.");
-            //}
-
-            switch (userGuess)
+            if (userGuess.Equals("7"))
             {
-                case "7":
-                    Console.WriteLine("You win!");
-                    break;
-                case "0":
-                    Console.WriteLine("Please guess a number between 1 and 10.");
-                    break;
-                default:
-                    Console.WriteLine("Try again.");
-                    break;
+                Console.WriteLine("You win!");
+            } else
+            {
+                Console.WriteLine("You lost.");
             }
+
+
+
+            //switch Attempt
+            //switch (userGuess)
+            //{
+            //    case "7":
+            //        Console.WriteLine("You win!");
+            //        break;
+            //    case "0":
+            //        Console.WriteLine("Please guess a number between 1 and 10.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Try again.");
+            //        break;
+            //}
         }
     }
 }
